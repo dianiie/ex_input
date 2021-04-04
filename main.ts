c327fec564cb23145655b96c64898fdfbadb8278
dianiie <81855246+dianiie@users.noreply.github.com>
2021-04-04 14:50:26 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 let pinSoil = 0
 let pinTouch = 0
+let pinFlame = 0
 basic.forever(function () {
     pinSoil = pins.digitalReadPin(DigitalPin.P1)
     pinTouch = pins.digitalReadPin(DigitalPin.P9)
-    serial.writeLine("" + (pinTouch))
+    pinFlame = pins.digitalReadPin(DigitalPin.P6)
+    serial.writeLine("" + (pinFlame))
     basic.pause(200)
 })
